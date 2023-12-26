e7313c1e309acf54f8e339df21514d073884e068
Simone CAPPELLETTI <simonecappelletti@tiscali.it>
2023-12-26 23:26:01 +0100

    Minor improvements

--- LeetCode/LeetCodeBusiness.cs
+++ LeetCode/LeetCodeBusiness.cs
@@ -230,15 +230,13 @@
             {
                 if (list1 is null)
                 {
-                    dummy.next = new ListNode(list2.val);
-
-                    list2 = list2.next;
+                    dummy.next = list2;
+                    break;
                 }
                 else if (list2 is null)
                 {
-                    dummy.next = new ListNode(list1.val);
-
-                    list1 = list1.next;
+                    dummy.next = list1;
+                    break;
                 }
                 else if (list1.val > list2.val)
                 {
